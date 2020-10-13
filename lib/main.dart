7b3eea6576df0c78014d21ddc0f98d95901cc046
DibyajyotiMishra <dibyajyotimishra14@gmail.com>
2020-10-13 11:15:38 +0530

    Chapter 1

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+      primarySwatch: Colors.purpleAccent[50],
+    ),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hii"),
+      ),
+      body: Container(
+        child: Text("This is A Flutter App"),
+      ),
+    );
+  }
+}
